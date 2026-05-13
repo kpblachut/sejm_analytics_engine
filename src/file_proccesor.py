85f70263c49de65b8d60e40bf50289d0e6aa1d91
kpblachut <kpblachut@gmail.com>
2026-05-13 22:10:31 +0200

    refactor: enhance voting data processing and structure, including new columns and exploded votes

--- src/file_proccesor.py
+++ src/file_proccesor.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import input_file_name, regexp_extract, col
+from pyspark.sql.functions import concat, explode, input_file_name, regexp_extract, col, lit, sha2
 
 spark = SparkSession.builder.appName("GovTransform").master("local[*]").getOrCreate()
 
@@ -17,10 +17,19 @@ df_mps_raw = spark.read \
     .withColumn(
         "term",
         regexp_extract("fileName", r"term(\d+)", 1)
+    ) \
+    .withColumn(
+        "mp_id_prehash",
+        concat(col("firstName"), lit("_"), col("lastName"), lit("_"), col("birthDate"), lit("_"), col("birthLocation"))
+    ) \
+    .withColumn(
+        "person_id",
+        sha2(col("mp_id_prehash"), 256)
     )
 
 df_mps_clean = df_mps_raw \
     .select(
+        col("person_id"),
         col("id").cast("int").alias("mp_id"),
         col("firstName").alias("first_name"),
         col("lastName").alias("last_name"),
@@ -41,32 +50,39 @@ df_votings_raw = spark.read \
     .format("json") \
     .option("recursiveFileLookup", "true") \
     .option("multiLine", "true") \
-    .load("../data/raw/votings/") \
-    .withColumn(
-        "file_name", 
-        input_file_name()
-    ) \
-    .withColumn(
-        "term", 
-        regexp_extract("file_name", r"term(\d+)", 1)
-    ) \
-    .withColumn(
-        "proceeding",
-        regexp_extract("file_name",r"proceeding(\d+)", 1)
-    ) \
-    .withColumn(
-        "vote_id", 
-        regexp_extract("file_name", r"vote(\d+)", 1)
+    .load("../data/raw/votings/")
+
+df_voting_meta = df_votings_raw \
+    .select(
+        col("abstain"),
+        col("date"),
+        col("description"),
+        col("kind"),
+        col("majorityType"),
+        col("majorityVotes"),
+        col("no"),
+        col("notParticipating"),
+        col("present"),
+        col("sitting"),
+        col("sittingDay"),
+        col("notParticipating"),
+        col("term"),
+        col("title"),
+        col("topic"),
+        col("totalVoted"),
+        col("votingNumber"),
+        col("yes")
     )
 
-df_votings_clean = df_votings_raw \
-    .select(
-        col("MP").cast("int").alias("mp_id"),
-        col("vote"),
-        col("term").cast("int"),
-        col("proceeding").cast("int"),
-        col("vote_id").cast("int")
-    )
+df_votings_exploded = df_votings_raw.withColumn("votes_exploded", explode(col("votes")))
+
+df_votes = df_votings_exploded.select(
+    col("votes_exploded.MP").alias("MP"),
+    col("votes_exploded.vote").alias("vote"),
+    col("term"),
+    col("sitting"),
+    col("votingNumber")
+)
 
 df_mps_clean.write \
     .mode("overwrite") \
@@ -74,10 +90,16 @@ df_mps_clean.write \
     .partitionBy("term") \
     .parquet("../data/silver/mps")
 
-df_votings_clean.write \
+df_voting_meta.write \
     .mode("overwrite") \
     .option("compression", "snappy") \
     .partitionBy("term") \
-    .parquet("../data/silver/votings")
+    .parquet("../data/silver/votings_meta")
+
+df_votes.write \
+    .mode("overwrite") \
+    .option("compression", "snappy") \
+    .partitionBy("term") \
+    .parquet("../data/silver/votes")
 
 spark.stop()
